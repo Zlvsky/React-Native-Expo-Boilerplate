b82e6c561b69031e29ac983c2da9235c1ca25332
Zlvsky <zlvsky@icloud.com>
2026-03-15 15:25:02 +0100

    fix styling tailwind

--- App.tsx
+++ App.tsx
@@ -1,23 +1,25 @@
+import 'react-native-reanimated'
 import './global.css'
 import './i18n/config'
-import 'react-native-reanimated'
 
+import { ThemeProvider } from '@/contexts/theme-context'
+import RootNavigator from '@/navigation/RootNavigator'
 import { NavigationContainer } from '@react-navigation/native'
 import { PortalHost } from '@rn-primitives/portal'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import RootNavigator from '@/navigation/RootNavigator'
-
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-        <PortalHost />
-      </SafeAreaProvider>
+      <ThemeProvider>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+          <PortalHost />
+        </SafeAreaProvider>
+      </ThemeProvider>
     </GestureHandlerRootView>
   )
 }
